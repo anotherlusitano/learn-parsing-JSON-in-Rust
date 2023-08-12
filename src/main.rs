2bfc978468c4cf1aa315dc09b9d54f045399c30f
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-08-12 15:03:58 +0100

    Fetch and print todos request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let todos = reqwest::Client::new()
+        .get("https://jsonplaceholder.typicode.com/todos?userId=1")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("{:#?}", todos);
+    Ok(())
 }
